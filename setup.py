3e174d75c570edc7b18c7f5561fa75ec268a9109
David C Hansen <dae.hansen@gmail.com>
2019-05-10 16:17:47 -0400

    Changes for upload to pip

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='ismrmrd-viewer',
     version='0.0.1',
     packages=['ismrmrd-viewer'],
-    package_dir={'ismrmrd-viewer': 'src/'},
+    package_dir={'ismrmrd-viewer': 'src'},
     license='LICENSE.txt',
     author='Kristoffer Langeland Knudsen',
     author_email='kristofferlknudsen@gradientsoftware.net',
     description='Simple tool for viewing ISMRMRD data.',
-    scripts=['ismrmrd-viewer']
+    scripts=['src/ismrmrd-viewer']
 )
