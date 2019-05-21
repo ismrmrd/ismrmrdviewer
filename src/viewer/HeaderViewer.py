6e3b1e9a2006cc704d7e858d3913d6805dcdb417
Matthew Restivo <mcr4u@virginia.edu>
2019-05-21 16:18:50 -0400

    Added HeaderViewer as TreeWidget

--- src/viewer/HeaderViewer.py
+++ src/viewer/HeaderViewer.py
@@ -1,24 +1,39 @@
-
 import logging
+import xml.etree.ElementTree as ET
 
-from PySide2 import QtWidgets
+from PySide2 import QtWidgets, QtCore
 
 
-class HeaderViewer(QtWidgets.QPlainTextEdit):
+class HeaderViewer(QtWidgets.QTreeWidget):
 
     def __init__(self, container):
         super().__init__()
 
         self.container = container
 
-        self.setReadOnly(True)
-        self.setTabChangesFocus(True)
-        self.setLineWrapMode(QtWidgets.QPlainTextEdit.LineWrapMode.NoWrap)
-        self.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.SizeAdjustPolicy.AdjustToContents)
-
         dom = container.header.toDOM()
-        xml = dom.toprettyxml(indent=4*' ')
+        xml = dom.toprettyxml(indent=4 * ' ')
+        root = ET.fromstring(xml)
 
-        self.setPlainText(xml)
+        item = QtWidgets.QTreeWidgetItem(self)
+        item.setText(0, root.tag)
+        self.addTopLevelItem(item)
+        item.setExpanded(True)
+        self.populate(item, root)
+        self.setColumnCount(2)
+        self.setHeaderLabels(("Parameter", "Value"))
+
+    def populate(self, item, node):
+        for child in node:
+            child_item = QtWidgets.QTreeWidgetItem(item)
+            child_item.setText(0, child.tag[30:])
+            if (len(child) == 0):
+                child_item.setData(1, QtCore.Qt.EditRole, child.text)
+                item.addChild(child_item)
+                child_item.setExpanded(True)
+            else:
+                item.addChild(child_item)
+                self.populate(child_item, child)
+                child_item.setExpanded(True)
 
 
